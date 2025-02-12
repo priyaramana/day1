6c852a6b84f3c6ad51ade5cc4a746c9a80d277ea
umadevitenapalli <umadevitenapalli@gmail.com>
2025-02-12 22:29:04 +0530

    Day3

---
+++ src/Day3/multiplication.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		// multiplication table
+		int n = 5;
+		int multiply;
+		for(int i=1;i<=10;i++) {
+			multiply = n*i;
+			System.out.println(n +"*" +i+ "=" +multiply);
+		}
+		
+
+	}
+
+}
---
+++ src/Day3/sum.java
@@ -0,0 +1,18 @@
+package Day3;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// sum of nums
+		int n = 5;
+		int sum = 0;
+		int i = 0;
+		while(i<=n) {
+			sum += i;
+			i++;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
